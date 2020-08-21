37e724cd76c2216c7b930322c149313a88776491
William Mantly <wmantly@gmail.com>
2020-08-21 17:03:55 -0400

    simpler CLI

--- app.js
+++ app.js
@@ -11,16 +11,14 @@ const exec_name = args[0].split('/').pop();
 const server_port = args[1];
 const clients_list = args.slice(2);
 
-if(server_port){
-	console.log('port:', server_port, 'clients:', clients_list)
-
-}else{
+if(server_port === "help"){
 	console.error('Please supply the server port and list of clients to connect too;')
 	console.error(`${exec_name} <server port> <client 1> <client 2> <client 3> ...` )
 	console.error(`${exec_name} 7575 10.1.0.1:7575 10.2.0.1:7575 10.3.0.1:7575` )
 	process.exit(0)
 }
 
+console.log('port:', server_port, 'clients:', clients_list)
 
 app.p2p = new P2P({
 	listenPort: server_port,
